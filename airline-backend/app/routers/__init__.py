0c25b0ceacd5ce5318494c1eb89d1d8b5e139b1a
Fatehin03 <fatehinalam03@gmail.com>
2026-01-30 20:45:40 +0600

    Create __init__.py

---
+++ airline-backend/app/routers/__init__.py
@@ -0,0 +1,5 @@
+from .auth import router as auth_router
+from .flight import router as flight_router
+from .booking import router as booking_router
+
+# This allows you to import them all at once in main.py
