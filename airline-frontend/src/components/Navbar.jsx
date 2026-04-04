baaa900d5f1bd5183f757fec4f28c0b25d34f189
Md. Shohanur Rrahaman Sowrav <sowravm72@gmail.com>
2026-04-04 22:34:38 +0600

    Update Navbar logo class for styling

--- airline-frontend/src/components/Navbar.jsx
+++ airline-frontend/src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
           <img
             src={brandLogo}
             alt="Skylink Airlines"
-            className="h-20 w-auto object-contain"
+            className="h-20"
           />
         </Link>
 
