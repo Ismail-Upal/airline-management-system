48202cfe3d2c3864837507acd8f9f2571fdcf758
Fatehin03 <fatehinalam03@gmail.com>
2026-04-16 12:14:30 +0600

    Update auth.py

--- airline-backend/app/routers/auth.py
+++ airline-backend/app/routers/auth.py
@@ -13,8 +13,8 @@ from app.core.security import (
     verify_password,
     get_password_hash,
     create_access_token,
-    get_current_user
 )
+from app.core.security import get_current_user
 from app.core.settings import settings
 from app.utils.email import send_reset_email
 from app.schemas.user import UpdateProfileSchema
